7e74903cf10048b60523de40f4c6b6bdf1c5332d
smolyAS <smoljakov@mail.ru>
2023-09-23 08:37:34 +0300

    add new class GCD (step 7)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,6 +11,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -25,6 +26,9 @@ public class App {
                 break;
             case 3:
                 Calc.gameCalc();
+                break;
+            case 4:
+                GCD.gameGCD();
                 break;
             case 0:
                 System.out.print("Exit");
---
+++ app/src/main/java/hexlet/code/GCD.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void gameGCD() {
+        Scanner scanner = new Scanner(System.in);
+
+        Engine.gameGreet();
+        Engine.nameGamer();
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+    }
+}
