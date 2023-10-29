67a0a6d3778fe3aec35a7557f4a8957b23f7be20
smolyAS <smoljakov@mail.ru>
2023-10-29 12:21:13 +0300

    code refactoring according to the review of October 25, items 3

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -9,23 +9,18 @@ public class Even {
     public static final int ROUNDS = 3;
     public static final int MIN_RANDOM = 0;
     public static final int MAX_RANDOM = 100;
-    private static int randomNum;
 
     public static void playEven() {
-
         Cli.greetingNewGamer();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-        randomNum = Utils.getRandomInt(MIN_RANDOM, MAX_RANDOM);
+        String[] questions = new String[ROUNDS];
+        String[] expectedAnswers = new String[ROUNDS];
 
-        int numberOfQuestions = ROUNDS;
-        String[] questions = new String[numberOfQuestions];
-        String[] expectedAnswers = new String[numberOfQuestions];
-
-        for (int i = 0; i < numberOfQuestions; i++) {
-
+        for (int i = 0; i < ROUNDS; i++) {
+            int randomNum = Utils.getRandomInt(MIN_RANDOM, MAX_RANDOM);
             String question = "Question: " + randomNum;
-            String expectedAnswer = checkEvenNumber();
+            String expectedAnswer = checkEvenNumber(randomNum) ? "yes" : "no";
 
             questions[i] = question;
             expectedAnswers[i] = expectedAnswer;
@@ -34,11 +29,7 @@ public class Even {
         Engine.run(questions, expectedAnswers);
     }
 
-    public static String checkEvenNumber() {
-        if (randomNum % 2 == 0) {
-            return "yes";
-        } else {
-            return "no";
-        }
+    public static boolean checkEvenNumber(int number) {
+        return number % 2 == 0;
     }
 }
